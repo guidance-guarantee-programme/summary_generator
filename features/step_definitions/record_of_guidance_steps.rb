ed0532d7e27e0edac80bf1d2c367cb26f3b712ff
Steven Wilkin <stevenwilkin@gmail.com>
2015-02-26 15:40:09 +0000

    Render output document as a PDF
    Displayed after appointment summary has been captured

--- features/step_definitions/record_of_guidance_steps.rb
+++ features/step_definitions/record_of_guidance_steps.rb
@@ -4,3 +4,9 @@ When(/^appointment details are captured$/) do
   page.name.set 'Joe Bloggs'
   page.submit.click
 end
+
+Then(/^a record of guidance document is created$/) do
+  expect(page.response_headers['Content-Type']).to eql('application/pdf')
+  text = PDF::Inspector::Text.analyze(page.source).strings.join
+  expect(text).to include('Joe Bloggs')
+end
