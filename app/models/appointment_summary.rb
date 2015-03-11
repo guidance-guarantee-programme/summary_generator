ac6affdc18629ae6707ed3adf4a223d4e06f88a7
Steven Wilkin <stevenwilkin@gmail.com>
2015-03-11 14:49:20 +0000

    Add reference_number to appointment_summaries

--- app/models/appointment_summary.rb
+++ app/models/appointment_summary.rb
@@ -1,7 +1,7 @@
 class AppointmentSummary
   include ActiveModel::Model
 
-  attr_accessor :name, :date_of_appointment,
+  attr_accessor :name, :date_of_appointment, :reference_number,
                 :value_of_pension_pots, :income_in_retirement,
                 :guider_name, :guider_organisation,
                 :continue_working, :unsure, :leave_inheritance,
