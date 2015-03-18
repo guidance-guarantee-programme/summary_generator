697e6a9bffe0daa9f583bf629af331930f57254a
Alistair Duggin <alistair.duggin@btinternet.com>
2015-03-18 15:54:19 +0000

    Remove extra style guide pages

--- app/controllers/styleguide_controller.rb
+++ app/controllers/styleguide_controller.rb
@@ -2,14 +2,7 @@ class StyleguideController < ApplicationController
   def index
   end
 
-  def base
-  end
-
   def pages_input
     render template: 'styleguide/pages/input'
-  end
-
-  def pages_input_v2
-    render template: 'styleguide/pages/input_v2'
   end
 end
