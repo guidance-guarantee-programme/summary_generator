2e5b7668cc34b97cfea6ec421bd756a47d43a4a1
Steven Wilkin <stevenwilkin@gmail.com>
2015-03-25 12:09:46 +0000

    Remove old routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
     scope path: 'styleguide', controller: 'styleguide' do
       scope path: 'pages' do
         get 'input', action: 'pages_input'
-        get 'input-v2', action: 'pages_input_v2'
       end
 
       get '(/:action)'
