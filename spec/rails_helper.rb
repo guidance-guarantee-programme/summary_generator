650ea19e5225d80b0d0d8f7dbbbb15bb74f3e331
Gareth Visagie <gareth@gjvis.com>
2015-03-26 11:21:34 +0000

    Initialize DigestAuthentication on startup
    - Looks for the user file at ENV['PDF_OUTPUT_USER_FILE']

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,4 +7,6 @@ require 'rspec/rails'
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.infer_spec_type_from_file_location!
+
+  config.before(:context, type: :request) { DigestAuthentication.reset }
 end
