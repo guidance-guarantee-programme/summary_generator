a9473b7062ba2f14ace4d1cacd66dd5f4f4662d4
Gareth Visagie <gareth@gjvis.com>
2015-04-10 11:50:47 +0100

    @wip ineligibility feature, and add step dfns

---
+++ features/step_definitions/ineligibility_letter_steps.rb
@@ -0,0 +1,11 @@
+Given(/^the customer doesn't have a defined contribution pension pot$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^they have had a Pension Wise appointment$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^we should send them an ineligibility letter$/) do
+  pending # express the regexp above with the code you wish you had
+end
