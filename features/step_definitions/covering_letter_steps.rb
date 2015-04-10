0c474617d762133a4bec11a0c214b0c22df8496d
Gareth Visagie <gareth@gjvis.com>
2015-04-10 12:16:42 +0100

    Implement covering letter feature

--- features/step_definitions/covering_letter_steps.rb
+++ features/step_definitions/covering_letter_steps.rb
@@ -1,7 +1,12 @@
-When(/^we send a customer a record of guidance$/) do
-  pending # express the regexp above with the code you wish you had
+Given(/^a customer has had a Pension Wise appointment$/) do
+  @appointment_summary = fixture(:populated_appointment_summary)
 end
 
 Then(/^it should include a covering letter$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(page.source).to include('<!-- section: cover letter -->')
+
+  output_document = OutputDocument.new(@appointment_summary)
+  expect(page).to have_content(output_document.attendee_address)
+  expect(page).to have_content(output_document.attendee_name)
+  expect(page).to have_content(output_document.lead)
 end
