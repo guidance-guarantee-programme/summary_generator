e8dfe9096fd050835fe6692f3921cf2cd7ab687d
Steven Wilkin <stevenwilkin@gmail.com>
2015-11-09 17:58:28 +0000

    Fix styleguide

--- app/controllers/styleguide_controller.rb
+++ app/controllers/styleguide_controller.rb
@@ -35,19 +35,19 @@ class StyleguideController < ApplicationController
   AppointmentSummary = Struct.new(
     :eligible_for_guidance?, :date_of_appointment, :guider_name,
     :guider_organisation, :title, :first_name, :last_name,
-    :value_of_pension_pots, :upper_value_of_pension_pots, :income_in_retirement,
-    :custom_guidance?, :continue_working?, :unsure?, :leave_inheritance?,
-    :wants_flexibility?, :wants_security?, :wants_lump_sum?, :poor_health?,
-    :generic_guidance?, :address_line_2, :address_line_3, :town, :county,
-    :postcode
+    :value_of_pension_pots, :upper_value_of_pension_pots, :value_of_pension_pots_is_approximate?, :income_in_retirement,
+    :custom_guidance?, :continue_working, :unsure, :leave_inheritance,
+    :wants_flexibility, :wants_security, :wants_lump_sum, :poor_health,
+    :generic_guidance?, :address_line_1, :address_line_2, :address_line_3, :town, :county,
+    :postcode, :country
   )
 
   def render_output_document(eligible_for_guidance: true, custom_guidance: true, generic_guidance: false)
     appointment_summary = AppointmentSummary.new(
       eligible_for_guidance, Time.zone.today, 'Jimmy', 'Pension Wise', 'Mr', 'Joe',
-      'Bloggs', 35_000, nil, 'pension', custom_guidance, custom_guidance, false,
-      false, false, false, false, false, generic_guidance, '73c', 'Burmah St',
-      'Belfast', 'Antrim', 'BT9 1HA'
+      'Bloggs', 35_000, false, nil, 'pension', custom_guidance, custom_guidance, false,
+      false, false, false, false, false, generic_guidance, '73c', 'Burmah St', '',
+      'Belfast', 'Antrim', 'BT9 1HA', Countries.uk
     )
     output_document = OutputDocument.new(appointment_summary)
 
