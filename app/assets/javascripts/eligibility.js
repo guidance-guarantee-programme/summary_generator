guidance-guarantee-programme/output

Progressively enhance form with javascript

app/assets/javascripts/eligibility.js
@@ -1,0 +1,29 @@
+(function ($) {
+  'use strict';
+
+  window.ROG = window.ROG || {};
+
+  var eligibility = {
+    init: function () {
+      this.cache();
+      this.bindEvents();
+    },
+    cache: function () {
+      this.$wrapper = $('.display_if_eligible');
+      this.$input = $('input[name="appointment_summary[eligibility]"]');
+    },
+    bindEvents: function () {
+      var that = this;
+      this.$input.on( 'change', function () {
+        if (this.value !== 'no') {
+          that.$wrapper.fadeIn();
+        } else {
+          that.$wrapper.fadeOut();
+        }
+      });
+    }
+  };
+
+  ROG.eligibility = eligibility;
+
+})(jQuery);

app/assets/javascripts/init.js
@@ -1,0 +1,7 @@
+$(function () {
+  ROG.consent.init();
+  ROG.eligibility.init();
+  ROG.pensionPotAccuracy.init();
+  ROG.pensionPotInput.init();
+  ROG.pensionPotPreview.init();
+});

guidance-guarantee-programme/summary_generator

app/assets/javascripts/eligibility.js
@@ -1,0 +1,29 @@
+(function ($) {
+  'use strict';
+
+  window.PWO = window.PWO || {};
+
+  var eligibility = {
+    init: function () {
+      this.cache();
+      this.bindEvents();
+    },
+    cache: function () {
+      this.$wrapper = $('.display_if_eligible');
+      this.$input = $('input[name="appointment_summary[eligibility]"]');
+    },
+    bindEvents: function () {
+      var that = this;
+      this.$input.on('change', function () {
+        if (this.value !== 'no') {
+          that.$wrapper.fadeIn();
+        } else {
+          that.$wrapper.fadeOut();
+        }
+      });
+    }
+  };
+
+  PWO.eligibility = eligibility;
+
+})(jQuery);

app/assets/javascripts/init.js
@@ -1,0 +1,7 @@
+$(function () {
+  PWO.consent.init();
+  PWO.eligibility.init();
+  PWO.pensionPotAccuracy.init();
+  PWO.pensionPotInput.init();
+  PWO.pensionPotPreview.init();
+});