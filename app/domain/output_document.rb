guidance-guarantee-programme/output

Use a domain object to generate record of guidance

app/domain/output_document.rb
@@ -1,0 +1,22 @@
+class OutputDocument
+  attr_accessor :appointment_summary
+
+  def initialize(appointment_summary)
+    @appointment_summary = appointment_summary
+  end
+
+  def html
+    ERB.new(template).result(binding)
+  end
+
+  def pdf
+    WickedPdf.new.pdf_from_string(html)
+  end
+
+  private
+
+  def template
+    File.read(
+      File.join(Rails.root, 'app', 'templates', 'output_document.html.erb'))
+  end
+end

guidance-guarantee-programme/summary_generator

app/domain/output_document.rb
@@ -1,0 +1,22 @@
+class OutputDocument
+  attr_accessor :appointment_summary
+
+  def initialize(appointment_summary)
+    @appointment_summary = appointment_summary
+  end
+
+  def html
+    ERB.new(template).result(binding)
+  end
+
+  def pdf
+    Princely.new.pdf_from_string(html)
+  end
+
+  private
+
+  def template
+    File.read(
+      File.join(Rails.root, 'app', 'templates', 'output_document.html.erb'))
+  end
+end