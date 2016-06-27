fcab5f286b46a7c00ef693b43070e859f6d6faa0
David Henry <dw_henry@yahoo.com.au>
2016-06-27 14:59:07 +0100

    Add organisation admin/appointmnet_summaries
    For both CSV and web view and use `CsvGenerator` 
    implementation for generating CSV output.

--- app/services/appointment_summary_csv.rb
+++ app/services/appointment_summary_csv.rb
@@ -1,50 +1,30 @@
 # frozen_string_literal: true
-require 'csv'
-
-class AppointmentSummaryCsv
-  ATTRIBUTES = %w(
-    id
-    date_of_appointment
-    value_of_pension_pots
-    guider_name
-    reference_number
-    address_line_1
-    address_line_2
-    address_line_3
-    town
-    county
-    postcode
-    country
-    title
-    first_name
-    last_name
-    format_preference
-    appointment_type
-    has_defined_contribution_pension
-  ).freeze
-
-  def initialize(appointments)
-    @appointments = Array(appointments)
+class AppointmentSummaryCsv < CsvGenerator
+  def attributes # rubocop: disable MethodLength
+    %w(
+      id
+      date_of_appointment
+      value_of_pension_pots
+      guider_name
+      reference_number
+      address_line_1
+      address_line_2
+      address_line_3
+      town
+      county
+      postcode
+      country
+      title
+      first_name
+      last_name
+      format_preference
+      appointment_type
+      has_defined_contribution_pension
+      organisation
+    ).freeze
   end
 
-  def call
-    CSV.generate do |output|
-      output << ATTRIBUTES
-
-      appointments.each { |appointment| output << row(appointment) }
-    end
-  end
-  alias csv call
-
-  private
-
-  attr_reader :appointments
-
-  def row(appointment)
-    appointment
-      .attributes
-      .slice(*ATTRIBUTES)
-      .values
-      .map(&:to_s)
+  def row(record)
+    super + [record.user.organisation_slug.upcase]
   end
 end
